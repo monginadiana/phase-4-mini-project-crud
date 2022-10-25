25aee6a563b6345dbb723da9b9f1f40d88443807
monginadiana <dianahmongina2@gmail.com>
2022-10-25 13:32:49 +0300

    mini crud app

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,32 @@
+class SpicesController < ApplicationController
+    def index
+        render json: Spice.all
+    end
+    def create
+        spice=Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+    def update
+        spice=Spice.find_by(id:params[:id])
+        if spice
+            spice.update(spice_params)
+            render json: spice, status: :created
+        else
+            render json: {error: "spice not found"}
+        end
+
+    end
+    def destroy
+        spice=Spice.find_by(id:params[:id])
+        if spice
+            spice.destroy
+            head :no_content
+        else
+            render json: {error: "spice not found"}
+        end
+    end
+    private
+    def spice_params
+    params.permit(:title,:image,:description,:notes,:rating)
+    end
+end
